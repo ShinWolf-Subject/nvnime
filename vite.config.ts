eb0a15e9179fda9d2f2180b825382774431e14f6
NvLabs <rosmita4rramiban@gmail.com>
2026-01-05 06:05:28 +0700

    [v1.0.0] Fix API

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.sansekai.my.id',
+        target: process.env.API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       }
